90abb3dde931264a57a18d6442b3ba8ec7791e51
shashwat0802 <shashwatgupta4656@gmail.com>
2024-01-14 15:39:33 +0530

    change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add custom alias configuration
+    config.resolve.alias['@'] = path.join(__dirname, '.');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
