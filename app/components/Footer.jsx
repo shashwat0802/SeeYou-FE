f1435e09ed6abbc00b7ee265e53b3db22c64eb11
amarjyothi98 <amarjyothi526@gmail.com>
2024-01-17 20:14:06 +0530

    account manager section initialized

---
+++ app/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0 h-[200px] w-full bg-inherit text-white">
+      <hr className="opacity-20" />
+      <div className="text-[#8F9098] text-center mt-10">
+        <p>Copyright © 2023 SeeYou</p>
+        <p className="mt-5">All Rights Reserved | Terms and Conditions | Security | Billing | Privacy Policy| Contact</p>
+      </div>
+    </div>
+  );
+}
