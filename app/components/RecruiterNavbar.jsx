e2c8169eb5f41789ea30dd51caa381cbff4810ac
shashwat0802 <shashwatgupta4656@gmail.com>
2024-02-26 20:58:05 +0530

    feat: separate recruiet navbar

---
+++ app/components/RecruiterNavbar.jsx
@@ -0,0 +1,267 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import textLogoPath from '@/public/images/text-logo.svg';
+import placeholder from '@/public/images/placeholder.svg';
+import { GoHome } from 'react-icons/go';
+import { IoBriefcaseOutline } from 'react-icons/io5';
+import { CiBookmarkMinus, CiDiscount1, CiSearch, CiUser } from 'react-icons/ci';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { IoIosLogOut } from 'react-icons/io';
+import { FaChevronDown } from 'react-icons/fa';
+import { PiBuildingsBold } from 'react-icons/pi';
+
+const RecruiterNavbar = () => {
+  const handleLogout = () => {
+    Cookies.remove('jwt');
+    Cookies.remove('role');
+    router.push('/signin');
+  };
+  return (
+    <div>
+      <>
+        <div>
+          <input type="checkbox" id="menu-open" className="hidden" />
+
+          <header
+            className="bg-[#22242D] text-gray-100 flex justify-between md:hidden"
+            data-dev-hint="mobile menu bar"
+          >
+            <Link
+              href="#"
+              className={`block p-4 text-white font-bold whitespace-nowrap truncate`}
+            >
+              <Image src={textLogoPath} height={25} width={70} />
+            </Link>
+
+            <label
+              htmlFor="menu-open"
+              id="mobile-menu-button"
+              className="m-2 p-2 focus:outline-none hover:text-white hover:bg-gray-700 rounded-md"
+            >
+              <svg
+                id="menu-open-icon"
+                className="h-6 w-6 transition duration-200 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                id="menu-close-icon"
+                className="h-6 w-6 transition duration-200 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </label>
+          </header>
+
+          <aside
+            id="sidebar"
+            className="bg-[#22242D] text-gray-100 md:w-64 w-3/4 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto mt-12"
+            style={{ zIndex: '999' }}
+            data-dev-hint="sidebar; px-0 for frameless; px-2 for visually inset the navigation"
+          >
+            <div
+              className="flex flex-col space-y-8"
+              data-dev-hint="optional div for having an extra footer navigation"
+            >
+              <nav data-dev-hint="main navigation">
+                <Link
+                  href="/dashboard/recruiter"
+                  className="flex items-center space-x-4 mb-6 py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <GoHome />
+                  </span>
+
+                  <span>Dashboard</span>
+                </Link>
+                <Link
+                  href="/application/recruiter"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <IoBriefcaseOutline />
+                  </span>
+
+                  <span>Applications</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <CiSearch />
+                  </span>
+
+                  <span>Search Visible Candidates</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl font-bold">
+                    <CiBookmarkMinus />
+                  </span>
+
+                  <span>Saved Applications</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl font-bold">
+                    <CiDiscount1 />
+                  </span>
+
+                  <span>Plans & Pricing</span>
+                </Link>
+                <Link
+                  href="/notification"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <IoMdNotificationsOutline />
+                  </span>
+
+                  <span>Notifications</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <PiBuildingsBold />
+                  </span>
+
+                  <span>Company Page</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl">
+                    <CiUser />
+                  </span>
+
+                  <span>Account Manager</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-xl font-black">
+                    <IoSettingsOutline />
+                  </span>
+
+                  <span>Settings</span>
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="flex items-center space-x-4  py-2 mt-6 px-4 transition duration-200 hover:bg-gray-700 hover:text-[#FF616D] text-[#FF616D] text-base"
+                >
+                  <span className="text-xl">
+                    <IoIosLogOut />
+                  </span>
+
+                  <span>Logout</span>
+                </button>
+              </nav>
+            </div>
+          </aside>
+        </div>
+
+        {/* desktop recruiter navbar */}
+        <nav className="flex justify-between hidden md:flex items-center container mx-auto py-4">
+          <div>
+            <Image src={textLogoPath} height={45} width={80} />
+          </div>
+          <div className="flex items-center">
+            <ul className="flex">
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4 py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span>Dashboard</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span>Applications</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span>Saved Applications</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-2xl">
+                    <IoMdNotificationsOutline />
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center space-x-4  py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white text-base"
+                >
+                  <span className="text-2xl font-black">
+                    <IoSettingsOutline />
+                  </span>
+                </Link>
+              </li>
+            </ul>
+            <div className="grid grid-cols-5 p-2 bg-transparent w-48 border border-white-20 place-items-center rounded-[24px] ml-4">
+              <Image
+                src={placeholder}
+                height={40}
+                width={40}
+                className="mx-2"
+              />
+              <div className="text-xs col-span-3">
+                <p className="font-bold">Ciprian Lopata</p>
+                <p>My Account</p>
+              </div>
+              <button className="text-xs">
+                <FaChevronDown />
+              </button>
+            </div>
+          </div>
+        </nav>
+      </>
+    </div>
+  );
+};
+
+export default RecruiterNavbar;
--- app/dashboard/recruiter/page.jsx
+++ app/dashboard/recruiter/page.jsx
@@ -1,103 +1,218 @@
-"use client";
-import React, { useState } from "react";
-import Navbar from "../../components/Navbar";
+'use client';
+import React, { useState } from 'react';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 // This Dashboard needs to be visited later to integrate correctly with backend apis
 
 const JobPostsDataFetchedFromBackend = [
-  {jobId:"1",jobName:"UI Designer",location:"Brussel, Belgium",applications:0,type:"DRAFT",status:"Almost published"},
-  {jobId:"2",jobName:"UI Designer",location:"Brussel, Belgium",applications:0,type:"",status:"In Review"},
-  {jobId:"3",jobName:"Content Writer",location:"Brussel, Belgium",applications:556,type:"OPEN",status:"Expiring in 3 days"},
-  {jobId:"4",jobName:"Product Manager",location:"Brussel, Belgium",applications:256,type:"OPEN",status:""},
-  {jobId:"5",jobName:"Product Manager",location:"Brussel, Belgium",applications:56,type:"PAUSED",status:""}]
+  {
+    jobId: '1',
+    jobName: 'UI Designer',
+    location: 'Brussel, Belgium',
+    applications: 0,
+    type: 'DRAFT',
+    status: 'Almost published',
+  },
+  {
+    jobId: '2',
+    jobName: 'UI Designer',
+    location: 'Brussel, Belgium',
+    applications: 0,
+    type: '',
+    status: 'In Review',
+  },
+  {
+    jobId: '3',
+    jobName: 'Content Writer',
+    location: 'Brussel, Belgium',
+    applications: 556,
+    type: 'OPEN',
+    status: 'Expiring in 3 days',
+  },
+  {
+    jobId: '4',
+    jobName: 'Product Manager',
+    location: 'Brussel, Belgium',
+    applications: 256,
+    type: 'OPEN',
+    status: '',
+  },
+  {
+    jobId: '5',
+    jobName: 'Product Manager',
+    location: 'Brussel, Belgium',
+    applications: 56,
+    type: 'PAUSED',
+    status: '',
+  },
+];
 
-const TopTab = () =>
-{
-  const [check,setCheck] = useState(true);
+const TopTab = () => {
+  const [check, setCheck] = useState(true);
 
   return (
     <section className="grid grid-cols-2 -mx-2.5">
-        <label htmlFor="openPause" className="flex flex-col items-center" >
-          <input className="peer sr-only" type="radio" checked={check} name="openClosed" id="openPause" onChange={()=>setCheck(true)} />
-          <h1 className=" peer-checked:text-white text-[#35383F] shrink-0 text-lg  mb-3">Open & Paused(4)</h1>
-          <aside className="self-stretch bg-[#35383F] peer-checked:bg-white rounded-full mt-[1px] h-0.5 peer-checked:h-1 peer-checked:mt-0"></aside>
-        </label>
-        <label htmlFor="expiredClosed" className="flex flex-col items-center">
-          <input className="peer sr-only" type="radio" checked={!check} name="openClosed" id="expiredClosed" onChange={()=>setCheck(false)} />
-          <h1 className=" peer-checked:text-white text-[#35383F] text-lg  mb-3">Expired/Closed(0)</h1>
-          <aside className=" self-stretch bg-[#35383F] peer-checked:bg-white rounded-full mt-[1px] h-0.5 peer-checked:h-1 peer-checked:mt-0"></aside>
-        </label>
-      </section>
-  )
-}
-const Filters = () =>
-{
-  const [selectedFilter,setSelectedFilter] = useState("Recently Edited");
-  const filters = ["Recently Edited","In Review","Open","Paused","Expiring Soon","Ascending (A to Z)","Descending (Z to A)"];
+      <label htmlFor="openPause" className="flex flex-col items-center">
+        <input
+          className="peer sr-only"
+          type="radio"
+          checked={check}
+          name="openClosed"
+          id="openPause"
+          onChange={() => setCheck(true)}
+        />
+        <h1 className=" peer-checked:text-white text-[#35383F] shrink-0 text-lg  mb-3">
+          Open & Paused(4)
+        </h1>
+        <aside className="self-stretch bg-[#35383F] peer-checked:bg-white rounded-full mt-[1px] h-0.5 peer-checked:h-1 peer-checked:mt-0"></aside>
+      </label>
+      <label htmlFor="expiredClosed" className="flex flex-col items-center">
+        <input
+          className="peer sr-only"
+          type="radio"
+          checked={!check}
+          name="openClosed"
+          id="expiredClosed"
+          onChange={() => setCheck(false)}
+        />
+        <h1 className=" peer-checked:text-white text-[#35383F] text-lg  mb-3">
+          Expired/Closed(0)
+        </h1>
+        <aside className=" self-stretch bg-[#35383F] peer-checked:bg-white rounded-full mt-[1px] h-0.5 peer-checked:h-1 peer-checked:mt-0"></aside>
+      </label>
+    </section>
+  );
+};
+const Filters = () => {
+  const [selectedFilter, setSelectedFilter] = useState('Recently Edited');
+  const filters = [
+    'Recently Edited',
+    'In Review',
+    'Open',
+    'Paused',
+    'Expiring Soon',
+    'Ascending (A to Z)',
+    'Descending (Z to A)',
+  ];
 
   return (
     <section className="flex justify-between relative">
-        <h1 className="text-sm font-extrabold">2 Jobs Posted</h1>
-        <button className="group outline-none text-left">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.3} stroke="currentColor" className="size-4 text-white group-focus:text-success">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-          </svg>
-          <ul className="absolute z-10 top-6 right-0 p-3 bg-bg-light rounded-2xl text-sm text-white-light hidden group-focus:block" >
-          {filters.map((filter,index)=>(
-            <li key={index} className={` ${filter===selectedFilter?"bg-primary-light":"hover:bg-primary-light"} rounded-xl py-4 px-3`} onClick={()=>setSelectedFilter(filter)}>{filter}</li>
+      <h1 className="text-sm font-extrabold">2 Jobs Posted</h1>
+      <button className="group outline-none text-left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2.3}
+          stroke="currentColor"
+          className="size-4 text-white group-focus:text-success"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+          />
+        </svg>
+        <ul className="absolute z-10 top-6 right-0 p-3 bg-bg-light rounded-2xl text-sm text-white-light hidden group-focus:block">
+          {filters.map((filter, index) => (
+            <li
+              key={index}
+              className={` ${
+                filter === selectedFilter
+                  ? 'bg-primary-light'
+                  : 'hover:bg-primary-light'
+              } rounded-xl py-4 px-3`}
+              onClick={() => setSelectedFilter(filter)}
+            >
+              {filter}
+            </li>
           ))}
         </ul>
-        </button>
-        
-      </section>
-  )
-}
-const TagSelector = ({tag}) =>
-{
-  switch(tag) {
-    case "Almost published":
-      return <span className="bg-white text-xs underline text-primary p-1.5 rounded-lg font-medium">{tag}</span>
+      </button>
+    </section>
+  );
+};
+const TagSelector = ({ tag }) => {
+  switch (tag) {
+    case 'Almost published':
+      return (
+        <span className="bg-white text-xs underline text-primary p-1.5 rounded-lg font-medium">
+          {tag}
+        </span>
+      );
 
-    case "In Review":
-      return <span className="text-xs text-[#2897FF] bg-[#2897FF2E] p-1.5 rounded-lg tracking-wide">{tag}</span>
+    case 'In Review':
+      return (
+        <span className="text-xs text-[#2897FF] bg-[#2897FF2E] p-1.5 rounded-lg tracking-wide">
+          {tag}
+        </span>
+      );
 
-    case "Expiring in 3 days":
-      return <span className="text-xs text-required bg-[#FF616D2E] p-1.5 rounded-lg tracking-wide underline">{tag}</span>
+    case 'Expiring in 3 days':
+      return (
+        <span className="text-xs text-required bg-[#FF616D2E] p-1.5 rounded-lg tracking-wide underline">
+          {tag}
+        </span>
+      );
 
-    case "PAUSED":
-      return <span>{tag}</span>
+    case 'PAUSED':
+      return <span>{tag}</span>;
 
     default:
-      return ""  ;
+      return '';
   }
-}
-const PostedJobList=()=>
-{
+};
+const PostedJobList = () => {
   return (
     <ul>
-        {JobPostsDataFetchedFromBackend.map((jobPost)=>(
-          <li key={jobPost?.jobId} className="p-4 border-b border-[#35383F]">
-            <label htmlFor={jobPost?.jobId} className="flex items-center gap-x-4">
-              <input type="checkbox" id={jobPost?.jobId}/>
-              <section className="grow">
-                <h1 className="text-lg">{jobPost?.jobName}</h1>
-                <p className="text-xs tracking-wide text-white-medium">{jobPost?.location} 
-                <span className={`empty:hidden underline ml-1 ${jobPost?.type==="OPEN"?" text-success":jobPost?.type==="DRAFT"?"text-required":"text-[#2897FF]"}`}>{jobPost?.type}</span>
-                </p>
-                <aside className="flex space-x-1 mt-1">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-                  </svg>
-                  <span className="text-xs tracking-wide">{jobPost?.applications} Applications</span>
-                </aside>
-              </section>
-              <TagSelector tag={jobPost?.status} />
-            </label>
-          </li>
-        ))}
-      </ul>
-  )
-}
+      {JobPostsDataFetchedFromBackend.map((jobPost) => (
+        <li key={jobPost?.jobId} className="p-4 border-b border-[#35383F]">
+          <label htmlFor={jobPost?.jobId} className="flex items-center gap-x-4">
+            <input type="checkbox" id={jobPost?.jobId} />
+            <section className="grow">
+              <h1 className="text-lg">{jobPost?.jobName}</h1>
+              <p className="text-xs tracking-wide text-white-medium">
+                {jobPost?.location}
+                <span
+                  className={`empty:hidden underline ml-1 ${
+                    jobPost?.type === 'OPEN'
+                      ? ' text-success'
+                      : jobPost?.type === 'DRAFT'
+                      ? 'text-required'
+                      : 'text-[#2897FF]'
+                  }`}
+                >
+                  {jobPost?.type}
+                </span>
+              </p>
+              <aside className="flex space-x-1 mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                  />
+                </svg>
+                <span className="text-xs tracking-wide">
+                  {jobPost?.applications} Applications
+                </span>
+              </aside>
+            </section>
+            <TagSelector tag={jobPost?.status} />
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const RecruiterDashboard = () => {
   return (
@@ -108,23 +223,36 @@ const RecruiterDashboard = () => {
           <h1 className=" text-xs">Welcome</h1>
           <h1 className=" text-lg">Dave Dus</h1>
         </aside>
-        <button className="px-4 py-3 border-2 border-white-medium text-sm font-semibold text-white-medium rounded-xl"> 
-          +
-          Post a Job</button>
+        <button className="px-4 py-3 border-2 border-white-medium text-sm font-semibold text-white-medium rounded-xl">
+          + Post a Job
+        </button>
       </header>
 
       <aside className="px-4 py-3 rounded-xl space-x-4 flex items-center bg-primary-light">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="white" className="w-4 h-4">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={3}
+          stroke="white"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
         </svg>
-        <input className="outline-none grow bg-transparent text-sm" placeholder="Filter & Search Jobs"/>
+        <input
+          className="outline-none grow bg-transparent text-sm"
+          placeholder="Filter & Search Jobs"
+        />
       </aside>
       <TopTab />
       <Filters />
       <PostedJobList />
-
     </div>
-  )
+  );
 };
 
 export default RecruiterDashboard;
