8cf79c3a0b6c984e00144f3462e950fe411406b2
Wadad Parker <wadadparker5@gmail.com>
2024-03-18 03:28:56 +0530

    style: unlocked profile

--- app/components/search-candidate/CandidateProfile.jsx
+++ app/components/search-candidate/CandidateProfile.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { useData } from '../../search-candidate/SearchCandidateContext'
 
 const CandidateProfile = () => {
     const {state:{candidate},dispatch} = useData();
+    const [unlocked,setUnlocked] = useState(false);
+    // Write logic here for video then set unlocked state to true
   return (
     <div className='px-2 space-y-4'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 my-2" onClick={()=>dispatch({type:"BACK_TO_SEARCH_RESULTS",payload:""})}>
@@ -11,12 +13,16 @@ const CandidateProfile = () => {
 
         <section className='p-6 bg-bg-light rounded-3xl space-y-4'>
             <header className='space-x-2.5 flex items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 text-required`}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                {unlocked
+                ?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-success">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                 </svg>
-                <h1 className={` font-black text-required `}>Profile Locked</h1>
+                :<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 text-required`}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg>}
+                <h1 className={` font-black ${unlocked?"text-success":"text-required"}  `}>Profile Locked</h1>
             </header>
-            <p className='text-required text-sm'>*Watch minimum 30 seconds to unlock profile details</p>
+            {!unlocked && <p className='text-required text-sm'>*Watch minimum 30 seconds to unlock profile details</p>}
             <section className=' grid grid-cols-[auto,1fr,auto] gap-x-2 items-start ' >
                 <img src={candidate.pfpLink} alt="Candidate" className=' object-cover w-20 h-20 rounded' onClick={()=>dispatch({type:"SELECT_CANDIDATE_PROFILE",payload:candidate})} />
                 <aside className='space-y-1 hover:cursor-pointer' onClick={()=>dispatch({type:"SELECT_CANDIDATE_PROFILE",payload:candidate})}>
@@ -39,7 +45,7 @@ const CandidateProfile = () => {
             <p className=' line-clamp-2 text-[#8F9098]'>{candidate.intro}</p>
         </section>
 
-        <button className=' py-3 px-4 bg-required text-sm text-white font-semibold rounded-xl flex items-center w-full justify-center'>
+        <button className={` py-3 px-4 ${unlocked?"bg-success":"bg-required"} text-sm text-white font-semibold rounded-xl flex items-center w-full justify-center`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className={` w-4 h-4 mr-1.5 text-white`}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
             </svg>
