034b11df94436d8d0f35d56f1971ed50d8d011ed
amarjyothi98 <amarjyothi526@gmail.com>
2024-04-02 16:28:00 +0530

    chore : added the 30 s timer over the video | fixed the positioning of the play/pause button

--- app/components/VideoComponent.jsx
+++ app/components/VideoComponent.jsx
@@ -11,13 +11,16 @@ const VideoComponent = () => {
 
   const handleProgress = (progress) => {
     // console.log(progress.playedSeconds);
+    const seconds = Math.floor(progress.playedSeconds);
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    setPlayedSeconds(formattedSeconds);
     if (progress.playedSeconds >= 30) {
       setShowControls(true);
     }
   };
 
   const customControls = (
-    <div className="flex justify-center items-center absolute inset-0">
+    <div className="mx-auto transform -translate-y-[150px] w-fit h-fit">
       <button onClick={() => setPlaying(!playing)} className="text-white">
         {playing ? (
           <div className="w-[70px] h-[70px]"></div>
@@ -30,8 +33,15 @@ const VideoComponent = () => {
     </div>
   );
 
+  const timer = () => (
+    <div className="p-1 rounded-lg w-fit absolute right-1 top-5 border border-red-700 text-red-700 bg-white bg-opacity-50">
+      {playedSeconds}s
+    </div>
+  );
+
   return (
     <div className="relative w-[300px] h-[200px]">
+      {playedSeconds < 30 && timer()}
       <ReactPlayer
         url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
         playing={playing}
